7e875b7e9a23af90ad0dd928f4a4893f20553392
Lee Yiyuan <leeyiyuan@hotmail.com>
2014-05-15 23:20:50 +0800

    Decreased search depth. A smoother AI looks more impressive.

--- js/expectimax.js
+++ js/expectimax.js
@@ -6,5 +6,6 @@ function bestMove(log_grid)
         log_grid[0][0], log_grid[0][1], log_grid[0][2], log_grid[0][3],
         log_grid[1][0], log_grid[1][1], log_grid[1][2], log_grid[1][3],
         log_grid[2][0], log_grid[2][1], log_grid[2][2], log_grid[2][3],
-        log_grid[3][0], log_grid[3][1], log_grid[3][2], log_grid[3][3], 4);
+        log_grid[3][0], log_grid[3][1], log_grid[3][2], log_grid[3][3],
+	3);
 }
